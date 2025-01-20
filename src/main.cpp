145eea13cf4aae3d1e98224537a5f3c93e85f3e1
M4yfl0w3r <mr.mayflower15@gmail.com>
2025-01-20 21:21:43 +0100

    *

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,15 @@
 #include <print>
 
+#include "variable.hpp"
 #include "static_string.hpp"
 
 using namespace mfl;
-
-enum class Type 
-{
-    vec2,
-    vec3,
-    vec4,
-    sampler2D
-};
-
-enum class Keyword 
-{
-    uniform,
-    in
-};
-
-constexpr auto space{ static_string{ " " } };
-constexpr auto equal{ static_string{ "=" } };
-constexpr auto plus{ static_string{ "+" } };
-constexpr auto line_end{ static_string{ ";" } };
 
 auto print(const auto& thing) {
     for (auto e : thing) {
         std::print("{}", e);
     }
     std::println("");
-}
-
-template <auto static_name, Type t, Keyword key>
-struct variable 
-{
-    static constexpr auto name{ static_name };
-    static constexpr auto type{ t };
-    static constexpr auto keyword{ key };
-
-    static constexpr auto declaration{ 
-        concat(
-            to_static_string<key>(), 
-            space, 
-            to_static_string<type>(), 
-            space, 
-            name, 
-            line_end
-        ) 
-    };
-};
-
-template <auto n1, auto n2, Type t, Keyword key>
-consteval auto add(const variable<n1, t, key>& st, const variable<n2, t, key>& nd) {
-    return concat(st.name, space, plus, space, nd.name, line_end);
-}
-
-template <auto n1, auto n2, Type t, Keyword key>
-consteval auto assign(const variable<n1, t, key>& dest, const variable<n2, t, key>& src) {
-    return concat(dest.name, space, equal, space, src.name, line_end);
-}
-
-template <auto n1, auto N, Type t, Keyword key>
-consteval auto assign(const variable<n1, t, key>& dest, const static_string<N>& src) {
-    return concat(dest.name, space, equal, space, src);
 }
 
 auto main() -> int 
---
+++ src/variable.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "static_string.hpp"
+
+namespace mfl 
+{
+    enum class Type 
+    {
+        vec2,
+        vec3,
+        vec4,
+        sampler2D
+    };
+
+    enum class Keyword 
+    {
+        uniform,
+        in
+    };
+
+    static constexpr auto space{ static_string{ " " } };
+    static constexpr auto equal{ static_string{ "=" } };
+    static constexpr auto plus{ static_string{ "+" } };
+    static constexpr auto line_end{ static_string{ ";" } };
+
+    template <auto static_name, Type t, Keyword key>
+    struct [[nodiscard]] variable 
+    {
+        static constexpr auto name{ static_name };
+        static constexpr auto type{ t };
+        static constexpr auto keyword{ key };
+
+        static constexpr auto declaration{ 
+            concat(
+                to_static_string<key>(), 
+                space, 
+                to_static_string<type>(), 
+                space, 
+                name, 
+                line_end
+            ) 
+        };
+    };
+
+    template <auto n1, auto n2, Type t, Keyword key>
+    consteval auto add(const variable<n1, t, key>& st, const variable<n2, t, key>& nd) {
+        return concat(st.name, space, plus, space, nd.name, line_end);
+    }
+
+    template <auto n1, auto n2, Type t, Keyword key>
+    consteval auto assign(const variable<n1, t, key>& dest, const variable<n2, t, key>& src) {
+        return concat(dest.name, space, equal, space, src.name, line_end);
+    }
+
+    template <auto n1, auto N, Type t, Keyword key>
+    consteval auto assign(const variable<n1, t, key>& dest, const static_string<N>& src) {
+        return concat(dest.name, space, equal, space, src);
+    }
+}
+
