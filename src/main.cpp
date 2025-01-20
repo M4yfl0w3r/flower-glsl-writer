8b396180ec2c919dce0bc12741aa0472760d580c
M4yfl0w3r <mr.mayflower15@gmail.com>
2025-01-20 09:39:39 +0100

    *

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <print>
-#include <unordered_map>
+#include <type_traits>
+#include <magic_enum/magic_enum.hpp>
 
 #define STRINGIFY(x) #x
 #define TO_STR(x) STRINGIFY(x)
@@ -37,10 +38,10 @@ struct static_string
     char value[N] {};
 };
 
-constexpr auto space{ static_string{ " " } };
-constexpr auto equal{ static_string{ "=" } };
-constexpr auto plus{ static_string{ "+" } };
-constexpr auto line_end{ static_string{ ";" } };
+// constexpr auto space{ static_string{ " " } };
+// constexpr auto equal{ static_string{ "=" } };
+// constexpr auto plus{ static_string{ "+" } };
+// constexpr auto line_end{ static_string{ ";" } };
 
 template <std::size_t... len>
 consteval auto concat(const static_string<len>&... strings)
@@ -58,10 +59,6 @@ consteval auto concat(const static_string<len>&... strings)
     return static_string{ result };
 }
 
-consteval auto enum_to_string(Uniform_t type) {
-    return static_string("vec3");
-}
-
 consteval auto keyword_to_string(Keyword key) {
     return static_string("uniform");
 }
@@ -73,47 +70,54 @@ auto print(const auto& thing) {
     std::println("");
 }
 
-template <auto static_name, Uniform_t t>
-struct uniform 
-{
-    static constexpr auto name{ static_name };
-    static constexpr auto type{ t };
-    static constexpr auto key{ Keyword::Uniform };
-    static constexpr auto init_expression{ concat(keyword_to_string(key), space, enum_to_string(type), space, name, line_end) };
-};
-
-template <auto n1, auto n2, Uniform_t t>
-consteval auto add(const uniform<n1, t>& st, const uniform<n2, t>& nd) {
-    return concat(st.name, space, plus, space, nd.name, line_end);
+template <typename T> requires std::is_enum_v<T>
+auto to_static_string(T enum_value) {
+    return static_string{ magic_enum::enum_name(enum_value) };
 }
 
-template <auto n1, auto n2, Uniform_t t>
-consteval auto assign(const uniform<n1, t>& dest, const uniform<n2, t>& src) {
-    return concat(dest.name, space, equal, space, src.name, line_end);
-}
-
-template <auto n1, auto N, Uniform_t t>
-consteval auto assign(const uniform<n1, t>& dest, const static_string<N>& src) {
-    return concat(dest.name, space, equal, space, src);
-}
+// template <auto static_name, Uniform_t t>
+// struct uniform 
+// {
+//     static constexpr auto name{ static_name };
+//     static constexpr auto type{ t };
+//     static constexpr auto key{ Keyword::Uniform };
+//     static constexpr auto init_expression{ concat(to_static_string(key), space, to_static_string(type), space, name, line_end) };
+// };
+//
+// template <auto n1, auto n2, Uniform_t t>
+// consteval auto add(const uniform<n1, t>& st, const uniform<n2, t>& nd) {
+//     return concat(st.name, space, plus, space, nd.name, line_end);
+// }
+//
+// template <auto n1, auto n2, Uniform_t t>
+// consteval auto assign(const uniform<n1, t>& dest, const uniform<n2, t>& src) {
+//     return concat(dest.name, space, equal, space, src.name, line_end);
+// }
+//
+// template <auto n1, auto N, Uniform_t t>
+// consteval auto assign(const uniform<n1, t>& dest, const static_string<N>& src) {
+//     return concat(dest.name, space, equal, space, src);
+// }
 
 auto main() -> int 
 {
-    constexpr auto ambient_name{ static_string{ "ambient" } };
-    constexpr auto ambient{ uniform<ambient_name, Uniform_t::vec3>() };
+    // constexpr auto ambient_name{ static_string{ "ambient" } };
+    // constexpr auto ambient{ uniform<ambient_name, Uniform_t::vec3>() };
+    //
+    // constexpr auto diffuse_name{ static_string{ "diffuse" } };
+    // constexpr auto diffuse{ uniform<diffuse_name, Uniform_t::vec3>() };
+    //
+    // constexpr auto color_name{ static_string{ "color" } };
+    // constexpr auto color{ uniform<color_name, Uniform_t::vec3>() };
+    //
+    // constexpr auto add_res{ add(ambient, diffuse) };
+    // constexpr auto ass_res{ assign(color, diffuse) };
+    //
+    // constexpr auto test{ assign(color, add_res) };
+    //
+    // print(add_res);
+    // print(ass_res);
+    // print(test);
 
-    constexpr auto diffuse_name{ static_string{ "diffuse" } };
-    constexpr auto diffuse{ uniform<diffuse_name, Uniform_t::vec3>() };
-
-    constexpr auto color_name{ static_string{ "color" } };
-    constexpr auto color{ uniform<color_name, Uniform_t::vec3>() };
-
-    constexpr auto add_res{ add(ambient, diffuse) };
-    constexpr auto ass_res{ assign(color, diffuse) };
-
-    constexpr auto test{ assign(color, add_res) };
-
-    print(add_res);
-    print(ass_res);
-    print(test);
+    std::println("{}", to_static_string(Keyword::Uniform));
 }
--- xmake.lua
+++ xmake.lua
@@ -4,14 +4,14 @@ set_languages("c++26")
 add_rules("mode.debug", "mode.release")
 set_warnings("all", "error")
 
-add_requires("glm", "gtest")
+add_requires("glm", "gtest", "magic_enum")
 
 target("app")
     add_files("src/*.cpp")
-    add_packages("glm")
+    add_packages("glm", "magic_enum")
 
 target("test")
     set_kind("binary")
     add_files("test/*.cpp")
     add_includedirs("src")
-    add_packages("glm", "gtest")
+    add_packages("glm", "gtest", "magic_enum")
